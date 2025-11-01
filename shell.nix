c39f8f428accc4bf89080b700e65ba0b999cb991
Roshan Bhatia <rshnbhatia@gmail.com>
2025-11-01 14:33:41 -0700

    chore: fix shell.nix configuration

--- shell.nix
+++ shell.nix
@@ -1,29 +1,25 @@
-{ pkgs ? import <nixpkgs> {} }:
+{
+  pkgs ? import <nixpkgs> { },
+}:
 
 pkgs.mkShell {
   name = "cudgel-dev-shell";
 
   buildInputs = with pkgs; [
-    # Rust toolchain
     rustc
     cargo
     rustfmt
     clippy
     rust-analyzer
 
-    # PostgreSQL with pgvector
-    postgresql_16
-    pgvector
+    postgresql_18
+    postgresql18Packages.pgvector
 
-    # Development tools
     docker
     docker-compose
 
-    # Task automation
     go-task
 
-    # Other utilities
-    git
     pkg-config
     openssl
   ];
@@ -37,14 +33,10 @@ pkgs.mkShell {
     echo "  task refresh  - Rebuild and reinstall"
     echo ""
 
-    # Set environment variables
     export RUST_BACKTRACE=1
     export CARGO_TARGET_DIR="target"
-
-    # Add cargo bin to PATH
     export PATH="$HOME/.cargo/bin:$PATH"
   '';
 
-  # Environment variables
   RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
 }
